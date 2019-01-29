43f9b33e8ab92991557ec13f8cd641322d6f571b
Matheus Carvalho <matheus.carvalho80@outlook.com>
2019-01-29 02:48:33 -0200

    Implementing simple search

--- SalesWebMvc/Controllers/SalesRecordsController.cs
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -20,6 +20,12 @@ namespace SalesWebMvc.Controllers
 
         public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
         {
+            minDate = minDate ?? new DateTime(DateTime.Now.Year, 1, 1);
+            maxDate = maxDate ?? DateTime.Now;
+
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+
             var result = await _salesRecordService.FindByDateAsync(minDate, maxDate);  
 
             return View(result);
